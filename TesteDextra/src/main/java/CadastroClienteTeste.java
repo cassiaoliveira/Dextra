0adf207955e7888a1a73d3967967849bf25ef472
Grupo07 - Cassia de Oliveira <kcyaoliveira@gmail.com>
2018-07-13 07:58:11 -0300

    first commit

---
+++ TesteDextra/src/main/java/CadastroClienteTeste.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CadastroClienteTeste {
+ 
+	public static void main(String [] args) {
+		
+	/*  WebDriver driver = new FirefoxDriver();
+	//  System.setProperty("webdriver.gecko.driver", \\path to gecko driver);
+	  driver.get("https://www.americanas.com.br/");*/
+		
+		System.out.println("teste");
+	}
+	
+}
